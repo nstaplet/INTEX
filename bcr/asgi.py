6c8e37e0cb7e0b8b885a238a8f98ed73d51d4361
nstaplet <nstaplet7@gmail.com>
2020-12-04 16:03:24 -0700

    updated project name and created apps

--- bcr/asgi.py
+++ bcr/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for intex project.
+ASGI config for bcr project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'intex.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bcr.settings')
 
 application = get_asgi_application()
