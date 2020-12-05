0647370a41edf58d714294aac40170245117bc72
tateWilks <64288630+tateWilks@users.noreply.github.com>
2020-12-05 13:06:47 -0700

    made an algorithms.py file for other functions

--- applicant/algorithms.py
+++ applicant/algorithms.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# from .models import 
+# from .models import skills, 
 
+
+# will retrieve the skills from the listings and put them into a sorted list to suggest which skills are most in demand by companies
 def recommend_skills(request):
 
     recommended_skills = {}
+
+    skills_listings = skills_listings_class.objects.all()
+
+    skills_occurances = {}
+
+    for row in skills_listings:
+        if skills_listings.skill_id in skills_occurances:
+            skills_occurances[skills_listings.skill_id] += 1
+        else:
+            skills_occurances[skills_listings.skill_id] = 0
+    
+    skills_occurances_sorted = sorted(skills_occurances.items(), key=lambda x: x[1], reverse=True)
 
     context = {
         'rec_skills': recommended_skills
