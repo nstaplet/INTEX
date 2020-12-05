471eb17a1aeece8c106a2d9f2a87bab49ae29ee8
Qujohujo <quinnjpete@gmail.com>
2020-12-05 12:32:38 -0800

    Update models.py

--- applicant/models.py
+++ applicant/models.py
@@ -14,15 +14,15 @@ class applicant(models.Model):
 
 class offers_review(models.Model):
     review_id = models.IntegerField(primary_key=True)
-    applicant_id = models.ForeignKey(applicant, on_delete=models.CASCADE)
-    organization_id = models.ForeignKey(organization, on_delete=models.CASCADE)
+    applicant_id = models.ForeignKey(applicant.id, on_delete=models.CASCADE)
+    organization_id = models.ForeignKey(organization.organization_id, on_delete=models.CASCADE)
     compensation = models.DecimalField(max_digits=8, decimal_places=2)
     satisfaction = models.IntegerField()
 
 class message(models.Model):
     message_id = models.IntegerField(primary_key=True)
-    applicant_id = models.ForeignKey(applicant, on_delete=models.CASCADE)
-    mentor_id = models.ForeignKey(mentor, on_delete=models.CASCADE)
+    applicant_id = models.ForeignKey(applicant.id, on_delete=models.CASCADE)
+    mentor_id = models.ForeignKey(mentor.mentor_id, on_delete=models.CASCADE)
     content = models.CharField(max_length=300)
     timesent = models.DateTimeField(auto_now=True, auto_now_add=False)
 
