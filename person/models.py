0df26ad5be2b0aafc7fa2742d16d503bc5815a25
Qujohujo <quinnjpete@gmail.com>
2020-12-05 13:24:36 -0800

    fixed circular model error

---
+++ person/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class applicant(models.Model):
+    id = models.IntegerField(primary_key=True)
+    email = models.EmailField(max_length=100, unique=True)
+    username = models.CharField(max_length=25)
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    email_opt_in = models.BooleanField(default=True)
+    city = models.CharField(max_length=25)
